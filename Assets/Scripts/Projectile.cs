a77d7c1c954d1e305e118886be179721fd0441ab
Anton Duppils <anton.duppils@gmail.com>
2021-05-01 18:11:38 +0200

    add Damage system. add enemy. add garbage collection and destruction of projectiles.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float speed = 20f;
-    public float damage = 2f;
+    public int damage = 2;
+    public float duration = 1f;
     public Rigidbody2D rb;
+    public string sourceTag;
     void Start()
     {
         rb.velocity = transform.right * speed;
+        Destroy(gameObject, duration);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Enemy")
-        {
-            // other.gameObject.DealDamage(damage);
-            Debug.Log("Enemy hit: " + other.name);
+        string otherTag = other.gameObject.tag;
+        if (otherTag != sourceTag){
+            Debug.Log("Hit: " + other.name);
+            Defense defense = other.GetComponent<Defense>();
+            if (defense != null){
+                defense.TakeDamage(damage=damage);
+                Debug.Log(other.name + "takes " + damage + " damage.");
+            }
+            Destroy(gameObject);
         }
-        // Destroy(gameObject);
     }
 
 }
